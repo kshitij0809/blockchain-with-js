771463ad360680b04de109a96bcafc00367bbd74
rajathacker <kshitijpratap0809@gmail.com>
2018-01-22 05:17:22 +0530

    block chain working

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const SHA256=require('crypto-js/sha256');
+const SHA256=require("crypto-js/sha256");
 
 class Block{
-	construstor{index,timestamp,data,previousHash=''}{
+	constructor(index,timestamp,data,previousHash=''){
 		this.index=index;
 		this.timestamp=timestamp;
 		this.data=data;
@@ -9,9 +9,10 @@ class Block{
 		this.hash=this.calculateHash();
 	}
 	calculateHash(){
-        return SHA256(this.index+this.timestamp+JSON.stringify(this.data)+this.previousHash).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
 	}
 }
+
 
 class Blockchain{
 	constructor(){
@@ -21,5 +22,25 @@ class Blockchain{
 	createGenesisBlock(){
 		return new Block(0,"01/01/2018","genesis Block","0");
 	}
+
+
+	getlatestblock(){
+		return this.chain[this.chain.length-1];
+	}
+
+
+	addblock(newblock){
+		newblock.previousHash=this.getlatestblock().hash;
+		newblock.hash=newblock.calculateHash();
+		this.chain.push(newblock);
+	}
+
 }
 
+
+let kshitijcoin=new Blockchain();
+kshitijcoin.addblock(new Block(1,"01/01/2018",{amount:10}));
+kshitijcoin.addblock(new Block(2,"09/09/2018",{amount:1000}));
+
+
+console.log(JSON.stringify(kshitijcoin,null,4));
